0766ee0722cb705a2e365e2d3d8ab2b445a2ec90
unknown <abhishekshukla2711@gmail.com>
2025-12-18 14:34:19 +0530

    frontend work started

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Request
 from app.routes import ingest, scoring
-
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 app = FastAPI(
     title="AI Hiring Copilot",
     description="AI system to rank resumes against job descriptions",
@@ -15,3 +16,7 @@ def root():
     return {"status": "AI Hiring Copilot is running ✅"}
 
 app.include_router(ingest.router,prefix="/ingest",tags=["Ingestion"])
+
+app=FastAPI()
+app.mount("/static",StaticFiles(directory="static"),name="static")
+templates=Jinja2Templates(directory="templates")
